44ed01f891620a41a05f2f6295e6af2eb9ac6042
Ofelia <o.sestu.96@gmail.com>
2024-12-01 11:29:57 +0100

    Update ConnessioneDb.java

--- src/connessione/ConnessioneDb.java
+++ src/connessione/ConnessioneDb.java
@@ -9,9 +9,9 @@ public class ConnessioneDb {
     // Parametri di configurazione del DB
     private static final String NOME_DB = "junkin_space"; // nome database
     private static final int PORT = 3306; // porta
-    private static final String SERVER_NAME = "localhost"; // indirizzo server MySqlWorkbench
-    private static final String USERNAME = "root"; // username
-    private static final String PASSWORD = "JAITA124"; // password
+    private static final String SERVER_NAME = ""; // indirizzo server MySqlWorkbench
+    private static final String USERNAME = ""; // username
+    private static final String PASSWORD = ""; // password
 
     // Metodo per ottenere una nuova connessione al database
     public static Connection getCon() throws SQLException {
